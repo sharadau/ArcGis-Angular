e799a7f9f96d6289bcfe27a00e28fd5e355f0e09
sujata-patne <sujata.patne@gmail.com>
2015-06-18 14:14:06 +0530

    Updated Direction and Draw widgets.

--- src/directives/directionsWidget.js
+++ src/directives/directionsWidget.js
@@ -8,25 +8,42 @@ angular.module("arcgis-map")
             restrict:'E',
             scope:{
                 mapid:"@"
-
             },
             // define an interface for working with this directive
             controller: function ($scope, $element, $attrs) {
                 var mappromise = mapRegistry.get($scope.mapid);
-                mappromise.then(function(map){
+                mappromise.then(function(map) {
                     require([
-                        'esri/dijit/Directions'], function (Directions) {
+                        "esri/urlUtils", "esri/dijit/Directions"
+                    ], function (urlUtils, Directions) {
+                        //all requests could/should be routed through a proxy to avoid making people sign in...
+                    urlUtils.addProxyRule({
+                         urlPrefix: "route.arcgis.com",
+                         proxyUrl: "/sproxy/proxy.ashx"
+                    });
+                    urlUtils.addProxyRule({
+                         urlPrefix: "traffic.arcgis.com",
+                         proxyUrl: "/sproxy/proxy.ashx"
+                    });
+                        var geocoderOptions = {
+                            autoComplete: true,
+                            arcgisGeocoder: {
+                                url: "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer",
+                                name: "Esri World Geocoder",
+                                sourceCountry: "USA"
+                            }
+                        };
                         var directions = new Directions({
-                            map: map
-                        },"directionId");
-
-                        directions.startup();
-                    })
+                             map: map,
+                             routeTaskUrl: "http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Network/USA/NAServer/Route",
+                             showClearButton: true,
+                             geocoderOptions: geocoderOptions
+                         },"directionId");
+                         directions.startup();
+                     })
                 });
             },
             templateUrl:"../src/template/directionsWidget.html"
 
         };
-    }]);/**
- * Created by synerzip on 17/06/15.
- */
+    }]);
