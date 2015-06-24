23987d64e44d9e965312436fd93d009fe7a072ce
yogesh-patel <synerzip@mac-pro151.synerzipune.local>
2015-06-24 17:04:01 +0530

    Added Widget Configuration JSON

--- test/app.js
+++ test/app.js
@@ -4,9 +4,114 @@
 
 angular.module("myApp",["layout-containers","arcgis-map"])
     .controller('MapController', function ($scope) {
+        //Widget selection and order
+        $scope.widgetConfigs = {
+            "leftNavItems":[
+                {
+                    "name": "Layers",
+                    "iconClass": "mdi-maps-layers red-text accent-2",
+                    "id":"layerWidget",
+                    selected:true
+                },
+                {
+                    "name": "Legend",
+                    "iconClass": "mdi-maps-map purple-text accent-2",
+                    "id":"legendWidget",
+                    selected:false
+                },
+                {
+                    "name": "Basemap",
+                    "iconClass": "mdi-maps-satellite green-text accent-2",
+                    "id":"basemapWidget",
+                    selected:false
+                },
+                {
+                    "name": "Direction",
+                    "id":"directionWidget",
+                    "iconClass": "mdi-maps-navigation green-text accent-2",
+                     selected:false
+                },
+                {
+                    "name": "Chart",
+                    "id": "chartWidget",
+                    "iconClass": "mdi-av-equalizer blue-text accent-2"
+
+                },
+                {
+                    "name": "Credit",
+                    "id": "creditWidget",
+                    "iconClass": "mdi-social-school amber-text darken-3"
+
+                },
+                {
+                    "name": "Drawing",
+                    "id": "drawingWidget",
+                    "iconClass": "mdi-editor-format-paint green-text accent-2"
+                },
+                {
+                    "name": "Search",
+                    "id": "searchWidget",
+                    "iconClass": "small mdi-action-search green-text accent-2"
+                }
+            ],
+            "rightNavItems":[
+                {
+                    "name": "Chart",
+                    "id": "chartWidget",
+                    "iconClass": "mdi-av-equalizer blue-text accent-2"
+
+                },
+                {
+                    "name": "Credit",
+                    "id": "creditWidget",
+                    "iconClass": "mdi-social-school amber-text darken-3"
+
+                },
+                {
+                    "name": "Drawing",
+                    "id": "drawingWidget",
+                    "iconClass": "mdi-editor-format-paint green-text accent-2"
+                },
+                {
+                    "name": "Search",
+                    "id": "searchWidget",
+                    "iconClass": "small mdi-action-search green-text accent-2"
+                }
+            ]
+        };
+
+
+        $scope.leftSelectionState = new Array();
+
+        $scope.isLeftItemSelected = function(itemId){
+            //var itemSelected = false;
+            for (var item in $scope.widgetConfigs.leftNavItems){
+                if(item.id == itemId) {
+                    return item.selected;
+                }
+            }
+
+            return false;
+        };
+
+        $scope.selectLeft = function(itemId){
+            $scope.openContent();
+            var selectedItem;
+            for (var item in $scope.widgetConfigs.leftNavItems){
+                $scope.widgetConfigs.leftNavItems[item].selected = false;
+                if($scope.widgetConfigs.leftNavItems[item].id == itemId){
+                    selectedItem = $scope.widgetConfigs.leftNavItems[item];
+                }
+            }
+
+            selectedItem.selected = true;
+        }
+
         $scope.basemapselected = false;
         $scope.legendselected = false;
         $scope.layerseleted = true;
+
+
 
 
         //Right Panels
