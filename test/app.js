a6f2e0acc79f4d1c668b25d810a9057ecd70fc18
abhishek789 <abhishek.somani@synerzip.com>
2015-06-24 18:48:37 +0530

    Adding settings code

--- test/app.js
+++ test/app.js
@@ -2,7 +2,7 @@
  * Created by yogeshp on 6/12/2015.
  */
 
-angular.module("myApp",["layout-containers","arcgis-map"])
+angular.module("myApp",["layout-containers","arcgis-map","dndLists"])
     .controller('MapController', function ($scope) {
         //Widget selection and order
         $scope.widgetConfigs = {
@@ -29,7 +29,7 @@ angular.module("myApp",["layout-containers","arcgis-map"])
                     "name": "Direction",
                     "id":"directionWidget",
                     "iconClass": "mdi-maps-navigation green-text accent-2",
-                     selected:false
+                    selected:false
                 },
                 {
                     "name": "Chart",
@@ -106,12 +106,9 @@ angular.module("myApp",["layout-containers","arcgis-map"])
 
             selectedItem.selected = true;
         }
-
         $scope.basemapselected = false;
         $scope.legendselected = false;
         $scope.layerseleted = true;
-
-
 
 
         //Right Panels
@@ -203,6 +200,93 @@ angular.module("myApp",["layout-containers","arcgis-map"])
             $scope.directionselected = false;
             $scope.drawingselected = true;
         }
+        $scope.leftWidgets =
+            {
+                label: "LeftW",
+                allowedTypes: ['left','right'],
+                max: 6,
+                people: [
+                    {
+                        "name": "Layers",
+                        "iconClass": "mdi-maps-layers red-text accent-2",
+                        "id":"layerWidget",
+                        selected:true,
+                        "type":"left"
+                    },
+                    {
+                        "name": "Legend",
+                        "iconClass": "mdi-maps-map purple-text accent-2",
+                        "id":"legendWidget",
+                        selected:false,
+                        "type":"left"
+                    }
+                ]
+            };
+        $scope.rightWidgets =
+        {
+            label: "RightW",
+            allowedTypes: ['right','left'],
+            max: 6,
+            people: [
+
+            ]
+        };
+        $scope.lists = [
+            {
+                label: "widgets",
+                allowedTypes: ['left','right'],
+                max: 4,
+                people: [
+
+                    {
+                        "name": "Basemap",
+                        "iconClass": "mdi-maps-satellite green-text accent-2",
+                        "id":"basemapWidget",
+                        selected:false,
+                        "type":"left"
+                    },
+                    {
+                        "name": "Direction",
+                        "id":"directionWidget",
+                        "iconClass": "mdi-maps-navigation green-text accent-2",
+                        selected:false,
+                        "type":"right"
+                    },
+                    {
+                        "name": "Chart",
+                        "id": "chartWidget",
+                        "iconClass": "mdi-av-equalizer blue-text accent-2",
+                        "type":"right"
+
+                    },
+                    {
+                        "name": "Credit",
+                        "id": "creditWidget",
+                        "iconClass": "mdi-social-school amber-text darken-3",
+                        "type":"left"
+
+                    },
+                    {
+                        "name": "Drawing",
+                        "id": "drawingWidget",
+                        "iconClass": "mdi-editor-format-paint green-text accent-2",
+                        "type":"left"
+                    },
+                    {
+                        "name": "Search",
+                        "id": "searchWidget",
+                        "iconClass": "small mdi-action-search green-text accent-2",
+                        "type":"right"
+                    }
+
+                ]
+            }
+        ];
+
+        // Model to JSON for demo purpose
+        $scope.$watch('lists', function(lists) {
+            $scope.modelAsJson = angular.toJson(lists, true);
+        }, true);
 
     });
 
